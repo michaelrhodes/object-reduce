f36ef33237d961c7951e7d5dfb7986da7a2015a8
michaelrhodes <spam@michaelrhod.es>
2013-11-25 20:43:12 +1100

    0.0.2 Removed needless dependency.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var array = require('arrayify')
-
 module.exports = function(object, keys) {
-  keys = array(keys)
+  var type = Object.prototype.toString.call(keys)
+  var isArray = type === '[object Array]'
+  if (!isArray) {
+    keys = Array(keys)
+  }
   var key
   var n = keys.length
   var result = {}
